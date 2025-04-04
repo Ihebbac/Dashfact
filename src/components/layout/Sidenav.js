46225a53f9223293280b2bf852c6935d774fb6e0
Baccouche iheb <ihebbaccouch1999@gmail.com>
2025-04-04 18:23:54 +0200

    wooooow malla eni

--- src/components/layout/Sidenav.js
+++ src/components/layout/Sidenav.js
@@ -13,7 +13,7 @@
 // import { useState } from "react";
 import { Menu, Button } from "antd";
 import { NavLink, useLocation } from "react-router-dom";
-import logo from "../../assets/images/logo.png";
+import logo from "../../assets/images/abstract-logs.png";
 import { useEffect } from "react";
 import axios from "axios";
 
@@ -170,7 +170,7 @@ function Sidenav({ color }) {
     <>
       <div className="brand">
         <img src={logo} alt="" />
-        <span>TN PRIME Dashboard</span>
+        <span>Stock360</span>
       </div>
       <hr />
       <Menu theme="light" mode="inline">
